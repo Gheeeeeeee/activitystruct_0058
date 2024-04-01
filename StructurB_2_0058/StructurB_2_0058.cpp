013ff5e83dec8a254f655f2f6ed392ce9fc5d60a
gheeeeeeee <gheoputra55@gmail.com>
2024-04-01 16:02:20 +0700

    deklarasi struct

---
+++ StructurB_2_0058/StructurB_2_0058.cpp
@@ -0,0 +1,19 @@
+// StructurB_2_0058.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct mahasiswa {
+	string nim;
+	string nama;
+	string alamat;
+	int umur;
+
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
