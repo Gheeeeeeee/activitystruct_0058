5e445a36aa0d7f25704f1d9afb0bf237ec579f73
gheeeeeeee <gheoputra55@gmail.com>
2024-04-01 16:26:33 +0700

    menampilkan hasil dari cin yg telah di isi sebelumnya

--- StructurB_2_0058/StructurB_2_0058.cpp
+++ StructurB_2_0058/StructurB_2_0058.cpp
@@ -25,13 +25,22 @@ int main()
 	cin >> mhs.nama;
 
 	cout << " alamat mahasiswa :" << endl;
-	cout << "\t nama desa :";
+	cout << " \t nama desa :";
 	cin >> mhs.alamat.desa;
-	cout << "\t nama kota :";
+	cout << " \t nama kota :";
 	cin >> mhs.alamat.kota;
 
 	cout << " umur mahasiswa :";
 	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n NIM :" << mhs.nim;
+	cout << "\n nama : " << mhs.nama;
+	cout << "\n alamat : " ;
+	cout << "\n desa : " << mhs.alamat.desa;
+	cout << "\n kota : " << mhs.alamat.kota;
+	cout << "\n umur : " << mhs.umur;
 }
+
 
 
