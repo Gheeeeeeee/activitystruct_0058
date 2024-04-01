cb33aadee2a433350f4616961b92c9f0401df9b2
gheeeeeeee <gheoputra55@gmail.com>
2024-04-01 16:47:26 +0700

    fin

---
+++ structb_4_0058/structb_4_0058.cpp
@@ -0,0 +1,47 @@
+// structb_4_0058.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct detailalamat {
+	string desa;
+	string kota;
+};
+
+struct mahasiswa {
+	string nim;
+	string nama;
+	detailalamat alamat;
+	int umur;
+
+};
+int main()
+{
+	mahasiswa mhs [5];
+
+	for (int i = 0; i < 3; i++) {
+
+		cout << " nomor mahasiswa :";
+		getline(cin, mhs.nim);
+		cout << " nama mahasiswa :";
+		getline(cin, mhs.nama);
+
+		cout << " alamat mahasiswa :" << endl;
+		cout << " \t nama desa :";
+		cin >> mhs.alamat.desa;
+		cout << " \t nama kota :";
+		cin >> mhs.alamat.kota;
+
+		cout << " umur mahasiswa :";
+		cin >> mhs.umur;
+
+		cout << endl;
+		cout << "\n NIM :" << mhs.nim;
+		cout << "\n nama : " << mhs.nama;
+		cout << "\n alamat : ";
+		cout << "\n desa : " << mhs.alamat.desa;
+		cout << "\n kota : " << mhs.alamat.kota;
+		cout << "\n umur : " << mhs.umur;
+	}
+}
